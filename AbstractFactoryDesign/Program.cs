f36c901d52f524d1d54082639832f654101c4d61
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2022-05-24 16:04:58 +0300

    Add project files.

---
+++ AbstractFactoryDesign/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace AbstractFactoryDesign
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            S8Fabrikası s8Fabrika = new S8Fabrikası();
+            Telefon s8 = s8Fabrika.GetTelefon("S8", "2600mah", 4, 7);
+
+            Note8Fabrikası note8Fabrika = new Note8Fabrikası();
+            Telefon note8 = note8Fabrika.GetTelefon("Note8", "3600mah", 5, 7);
+
+            Console.WriteLine("S8 için telefon özellikleri");
+            Console.WriteLine(s8);
+            Console.WriteLine("________________________________________________");
+
+            Console.WriteLine("Note8 için telefon özellikleri");
+            Console.WriteLine(note8);
+
+
+            Console.ReadKey();
+        }
+    }
+    interface ITelefonFabrikası
+    {
+        Telefon GetTelefon(string model, string batarya, int en, int boy);
+    }
+
+    interface Telefon
+    {
+        string GetModel();
+        string GetBatarya();
+        int getEn();
+        int getBoy();
+    }
+    class S8Fabrikası : ITelefonFabrikası, Telefon
+    {
+        private string model { get; set; }
+        private string batarya { get; set; }
+        private int en;
+        private int boy;
+        public S8Fabrikası()
+        {
+
+        }
+        public S8Fabrikası(string model, string batarya, int en, int boy)
+        {
+            this.model = model;
+            this.batarya = batarya;
+            this.en = en;
+            this.boy = boy;
+        }
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public string GetBatarya()
+        {
+            return batarya;
+        }
+
+        public int getEn()
+        {
+            return en;
+        }
+
+        public int getBoy()
+        {
+            return boy;
+        }
+        public override string ToString()
+        {
+            return "S8 (" + "model='" + model + '\'' + ",batarya='" + batarya + '\'' + ", en=" + en + ", boy=" + boy + ')';
+        }
+
+        public Telefon GetTelefon(string model, string batarya, int en, int boy)
+        {
+            return new S8Fabrikası(model, batarya, en, boy);
+        }
+    }
+    class Note8Fabrikası : ITelefonFabrikası, Telefon
+    {
+        private string model { get; set; }
+        private string batarya { get; set; }
+        private int en;
+        private int boy;
+        public Note8Fabrikası()
+        {
+
+        }
+        public Note8Fabrikası(string model, string batarya, int en, int boy)
+        {
+            this.model = model;
+            this.batarya = batarya;
+            this.en = en;
+            this.boy = boy;
+        }
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public string GetBatarya()
+        {
+            return batarya;
+        }
+
+        public int getEn()
+        {
+            return en;
+        }
+
+        public int getBoy()
+        {
+            return boy;
+        }
+        public override string ToString()
+        {
+            return "Note8 (" + "model='" + model + '\'' + ",batarya='" + batarya + '\'' + ", en=" + en + ", boy=" + boy + ')';
+        }
+
+        public Telefon GetTelefon(string model, string batarya, int en, int boy)
+        {
+            return new Note8Fabrikası(model, batarya, en, boy);
+        }
+    }
+}
---
+++ FactoryDesign/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesign
+{//FactoryDesignPattern
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefon s8 = TelefonFabrikası.GetTelefon("S8", "2600mah", 4, 7);
+
+            Telefon note8 = TelefonFabrikası.GetTelefon("Note8", "3600mah", 5, 7);
+
+            Console.WriteLine("S8 için telefon özellikleri");
+            Console.WriteLine(s8);
+            Console.WriteLine("________________________________________________");
+
+            Console.WriteLine("Note8 için telefon özellikleri");
+            Console.WriteLine(note8);
+
+
+            Console.ReadKey();
+        }
+    }
+    class TelefonFabrikası
+    {
+        public static Telefon GetTelefon(string model, string batarya, int en, int boy)
+        {
+            Telefon telefon;
+
+            if (model.ToLower() == "S8".ToLower())
+            {
+                telefon = new S8(model, batarya, en, boy);
+            }
+            else if (model.ToLower() == "Note8".ToLower())
+            {
+                telefon = new Note8(model, batarya, en, boy);
+            }
+            else
+            {
+                throw new Exception("Geçerli bir model değildir.");
+            }
+            return telefon;
+        }
+    }
+    interface Telefon
+    {
+        string GetModel();
+        string GetBatarya();
+        int getEn();
+        int getBoy();
+    }
+    class S8 : Telefon
+    {
+        private string model { get; set; }
+        private string batarya { get; set; }
+        private int en;
+        private int boy;
+
+        public S8(string model, string batarya, int en, int boy)
+        {
+            this.model = model;
+            this.batarya = batarya;
+            this.en = en;
+            this.boy = boy;
+        }
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public string GetBatarya()
+        {
+            return batarya;
+        }
+
+        public int getEn()
+        {
+            return en;
+        }
+
+        public int getBoy()
+        {
+            return boy;
+        }
+        public override string ToString()
+        {
+            return "S8 (" + "model='" + model + '\'' + ",batarya='" + batarya + '\'' + ", en=" + en + ", boy=" + boy + ')';
+        }
+    }
+    class Note8 : Telefon
+    {
+        private string model { get; set; }
+        private string batarya { get; set; }
+        private int en;
+        private int boy;
+
+        public Note8(string model, string batarya, int en, int boy)
+        {
+            this.model = model;
+            this.batarya = batarya;
+            this.en = en;
+            this.boy = boy;
+        }
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public string GetBatarya()
+        {
+            return batarya;
+        }
+
+        public int getEn()
+        {
+            return en;
+        }
+
+        public int getBoy()
+        {
+            return boy;
+        }
+        public override string ToString()
+        {
+            return "Note8 (" + "model='" + model + '\'' + ",batarya='" + batarya + '\'' + ", en=" + en + ", boy=" + boy + ')';
+        }
+    }
+}
